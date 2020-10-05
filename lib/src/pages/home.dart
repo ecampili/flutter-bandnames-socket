de1dd3e968f4a21fb72f125ac9f36144416f7899
ecampili <eduardo.campili@gmail.com>
2020-10-05 09:41:45 -0300

    Fin seccion 2

---
+++ lib/src/pages/home.dart
@@ -0,0 +1,144 @@
+import 'dart:io';
+
+import 'package:band_names/src/models/band.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Band> bands = [
+    Band(id: '1', name: 'Pink Floyd', votes: 10),
+    Band(id: '1', name: 'Genesis', votes: 8),
+    Band(id: '1', name: 'Queen', votes: 7),
+    Band(id: '1', name: 'Yes', votes: 8),
+    Band(id: '1', name: 'EL&P', votes: 7),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Band Names',
+          style: TextStyle(color: Colors.black87),
+        ),
+        elevation: 1,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+        itemCount: bands.length,
+        itemBuilder: (context, index) => _bandTile(bands[index]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNewBand,
+        child: Icon(Icons.add),
+        elevation: 1,
+      ),
+    );
+  }
+
+  Widget _bandTile(Band band) {
+    return Dismissible(
+      key: Key(band.id),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction) {
+        print('direction: $direction');
+        print('id: ${band.id}');
+
+        //TODO: llamar el borrrado en el server
+      },
+      background: Container(
+        padding: EdgeInsets.only(left: 8.0),
+        color: Colors.red,
+        child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Delete Band',
+              style: TextStyle(color: Colors.white),
+            )),
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Text(band.name.substring(0, 2)),
+          backgroundColor: Colors.blue[100],
+        ),
+        title: Text(band.name),
+        trailing: Text(
+          '${band.votes}',
+          style: TextStyle(fontSize: 20),
+        ),
+        onTap: () {
+          print(band.name);
+        },
+      ),
+    );
+  }
+
+  addNewBand() {
+    final textController = TextEditingController();
+
+    // para ios platform debe ser importada de dart.io
+    if (Platform.isAndroid) {
+      // para android
+      return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('New band name:'),
+            content: TextField(
+              controller: textController,
+            ),
+            actions: [
+              MaterialButton(
+                child: Text('Add'),
+                elevation: 5,
+                color: Colors.blue,
+                onPressed: () => addBandToList(textController.text),
+              )
+            ],
+          );
+        },
+      );
+    }
+
+    showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text('New band Name'),
+            content: CupertinoTextField(
+              controller: textController,
+            ),
+            actions: [
+              CupertinoDialogAction(
+                isDefaultAction: true,
+                child: Text('Add'),
+                onPressed: () => addBandToList(textController.text),
+              ),
+              CupertinoDialogAction(
+                  isDestructiveAction: true,
+                  child: Text('Dismiss'),
+                  onPressed: () => Navigator.pop(context))
+            ],
+          );
+        });
+  }
+
+  void addBandToList(String name) {
+    print(name);
+    if (name.length > 1) {
+      // podemos agregar
+
+      this
+          .bands
+          .add(Band(id: DateTime.november.toString(), name: name, votes: 0));
+    }
+    setState(() {});
+
+    Navigator.pop(context);
+  }
+}
